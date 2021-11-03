b93ac8f6f7e38bc9e35ac989295397dbcac52248
aaug <aaug@caasco.ca>
2021-11-03 14:41:15 -0400

    setting attrubutes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends React.Component {
-    state = {
-        count: 0
-    };
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={{color:'black'}} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
 
   formatCount() {
-      const {count} = this.state;
-      return count === 0 ? 'Zero' : count;
+    const { count } = this.state;
+    return count === 0 ? "Zero " : count;
   }
 }
 
