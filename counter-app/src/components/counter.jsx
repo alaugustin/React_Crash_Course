32098b473f06ac2ce70bf09b9734d56ac881c019
aaug <aaug@caasco.ca>
2021-11-03 15:46:17 -0400

    dynamically rendering classes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -8,10 +8,25 @@ class Counter extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <span style={{color:'black'}} className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getSpanClasses()}>{this.formatCount()}</span>
+        <button className={this.getButtonClasses()}>Increment</button>
       </React.Fragment>
     );
+  }
+
+  getSpanClasses() {
+    let spanClasses =
+      "bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative";
+    spanClasses =
+      this.state.count === 0
+        ? "bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative"
+        : "bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative";
+    return spanClasses;
+  }
+
+  getButtonClasses() {
+    let buttonClasses = "bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded";
+    return buttonClasses;
   }
 
   formatCount() {
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'tailwindcss/dist/tailwind.css'
 import Counter from './components/counter';
 
 
