205208e17d25a1458f255f1062d2873f4d2274f8
aaug <aaug@caasco.ca>
2021-11-08 17:06:28 -0500

    cleaning up navbar

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,41 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class NavBar extends Component {
-    render() {
-        return (
-          <nav className={"container mx-auto"}>
+const NavBar = (props) => {
+  return (
+    <nav className={"p-2 bg-gray-300 "}>
+      <div className="container mx-auto flex justify-between">
+        <h1 className={"font-bold text-xl"}>React Counting App</h1>
 
-
-
-
-
-
-            <span className={"inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"}>{this.props.totalCounters}</span>
-            <ul class="flex">
-              <li class="mr-6">
-                <a class="text-blue-500 hover:text-blue-800" href="/">
-                  Active
-                </a>
-              </li>
-              <li class="mr-6">
-                <a class="text-blue-500 hover:text-blue-800" href="/">
-                  Link
-                </a>
-              </li>
-              <li class="mr-6">
-                <a class="text-blue-500 hover:text-blue-800" href="/">
-                  Link
-                </a>
-              </li>
-              <li class="mr-6">
-                <a class="text-gray-400 cursor-not-allowed" href="/">
-                  Disabled
-                </a>
-              </li>
-            </ul>
-          </nav>
-        );
-    }
-}
+        <span
+          className={
+            "items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"
+          }
+        >
+          {props.totalCounters}
+        </span>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'tailwindcss/dist/base.css';
 import 'tailwindcss/dist/components.css';
 import 'tailwindcss/dist/utilities.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
