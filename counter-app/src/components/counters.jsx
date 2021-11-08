987fd47df3548e992638958cc82e184443094b6e
aaug <aaug@caasco.ca>
2021-11-08 16:32:58 -0500

    lifting the state up

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -2,53 +2,23 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 6 },
-      { id: 3, value: 0 },
-      { id: 4, value: 100 },
-    ],
-  };
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({counters})
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <>
         <div className="my-2">
           <button
-            onClick={this.handleReset}
+            onClick={this.props.onReset}
             className={this.getResetButtonClasses()}
           >
             Reset
           </button>
         </div>
 
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           >
             <h3 className={"text-lg font-bold mb-2"}>Counter #{counter.id}</h3>
--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -4,6 +4,13 @@ class NavBar extends Component {
     render() {
         return (
           <nav className={"container mx-auto"}>
+
+
+
+
+
+
+            <span className={"inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full"}>{this.props.totalCounters}</span>
             <ul class="flex">
               <li class="mr-6">
                 <a class="text-blue-500 hover:text-blue-800" href="/">
