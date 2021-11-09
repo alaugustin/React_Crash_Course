b8b4bba09d61871ac19742efdc867ae9d1999e23
aaug <aaug@caasco.ca>
2021-11-09 09:11:54 -0500

    cleaning up branch

--- react-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const element = <h1>Hello World!</h1>;
-
-ReactDOM.render(element, document.getElementById('root'));
-
-console.log(element)
